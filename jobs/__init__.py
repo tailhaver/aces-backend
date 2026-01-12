29e4553cf1323a6795568fb056194b94eda00f4a
Niko Yu <137782638+thesleepyniko@users.noreply.github.com>
2026-01-12 00:56:45 -0500

    feat: add devlog sync test HTML page and update exports in jobs module

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,4 +1,9 @@
-from .runner import run_cleanup, run_pyramid_sync
+from .runner import run_cleanup, run_pyramid_sync, run_devlog_review_sync
 from .usercleanup import cleanup_deleted_users
 
-__all__ = ["cleanup_deleted_users", "run_cleanup", "run_pyramid_sync"]
+__all__ = [
+    "cleanup_deleted_users",
+    "run_cleanup",
+    "run_pyramid_sync",
+    "run_devlog_review_sync",
+]
