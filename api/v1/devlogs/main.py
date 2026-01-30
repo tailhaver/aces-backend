8f8e2833a4f5d26504b21599b781ee8539fc9a97
Niko Yu <137782638+thesleepyniko@users.noreply.github.com>
2026-01-29 19:22:39 -0500

    style: fmt

--- api/v1/devlogs/main.py
+++ api/v1/devlogs/main.py
@@ -198,7 +198,9 @@ async def create_devlog(
         .order_by(Devlog.id.desc())
         .limit(1)
     )
-    last_hours_snapshot = last_devlog_result.scalar_one_or_none() or 0 # prevent the none case
+    last_hours_snapshot = (
+        last_devlog_result.scalar_one_or_none() or 0
+    )  # prevent the none case
 
     if current_hours <= last_hours_snapshot:
         raise HTTPException(
